fbc70039ba9476a95ce0fa23bd80370a662c712c
Yehiyam Livneh <yehiyam@gmail.com>
2019-09-12 10:11:34 +0300

    incremental

--- tests/mocks/store-manager.js
+++ tests/mocks/store-manager.js
@@ -1,31 +1,50 @@
 const moment = require('moment');
+let webhooks = require('./webhooks.json');
+let jobStatus = require('./jobStatus.json');
+let jobResults = require('./jobResults.json');
 class StateManager {
     async init(options) {
+        this._deleted = {};
+        webhooks[0].timestamp = Date.now();
+        webhooks[1].timestamp = new Date(2018, 1, 1, 10, 33, 30, 0).getTime();
+        webhooks = [...webhooks, ...Array.from(Array(200)).map(j => ({ ...(webhooks[1]) })), ...Array.from(Array(200)).map(j => ({ ...(webhooks[0]) }))];
     }
 
     getKeys(path) {
         switch (path) {
             case "/webhooks":
-                let webhooks = require('./webhooks.json');
-                webhooks[0].timestamp = new Date();
-                webhooks[1].timestamp = new Date(2018, 1, 1, 10, 33, 30, 0);;
-                return webhooks.map(j => JSON.stringify(j));
+                return webhooks.map((j, i) => ({ ...j, i }))
+                .filter(j => !this._deleted[`/jobs/results/${j.i}`])
+                .sort((a,b)=>a-b)
+                .reduce((prev, cur) => {
+                    prev[`/webhooks/${cur.i}`] = JSON.stringify(cur);
+                    return prev;
+                }, {});
+                // return webhooks
+                //     .map((j, i) => ({ ...j, i ,key: `/jobs/results/${i}`}))
+                //     .filter(j => !this._deleted[j.key])
+                    
             case "/jobs/status":
-                let jobStatus = require('./jobStatus.json');
                 jobStatus[0].timestamp = new Date();
-                jobStatus[1].timestamp = new Date(2018, 1, 2, 10, 33, 30, 0);;
-                return jobStatus.map(j => JSON.stringify(j));
+                jobStatus[1].timestamp = new Date(2018, 1, 2, 10, 33, 30, 0);
+                return jobStatus.map((j, i) => ({ ...j, i })).filter(j => !this._deleted[`/jobs/results/${j.i}`]).reduce((prev, cur) => {
+                    prev[`/jobs/status/${cur.i}`] = JSON.stringify(cur);
+                    return prev;
+                }, {});
             case "/jobs/results":
-                let jobResults = require('./jobResults.json');
                 jobResults[0].timestamp = new Date();
-                jobResults[1].timestamp = new Date(2018, 1, 3, 10, 33, 30, 0);;
-                return jobResults.map(j => JSON.stringify(j));
+                jobResults[1].timestamp = new Date(2018, 1, 3, 10, 33, 30, 0);
+                return jobStatus.map((j, i) => ({ ...j, i })).filter(j => !this._deleted[`/jobs/results/${j.i}`]).reduce((prev, cur) => {
+                    prev[`/jobs/results/${cur.i}`] = JSON.stringify(cur);
+                    return prev;
+                }, {});
             default:
                 return [];
         }
     }
 
     deleteKey(key) {
+        this._deleted[key] = true;
         console.log(key);
     }
 }
