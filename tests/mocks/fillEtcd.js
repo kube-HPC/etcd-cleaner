e7541aa40f2000cb09615e448574994387feb5a9
yehiyam <yehiyam@users.noreply.github.com>
2020-05-17 16:04:13 +0300

    incrementally clean redis (#11)
    * incrementally clean redis

--- tests/mocks/fillEtcd.js
+++ tests/mocks/fillEtcd.js
@@ -1,5 +1,7 @@
 const Etcd = require('@hkube/etcd');
-const main = async () => {
+const { Factory } = require('@hkube/redis-utils');
+
+const fillEtcd = async () => {
     const SERVICE_NAME = 'my-test-service';
     const config = { host: 'localhost', port: '4001', serviceName: SERVICE_NAME };
     etcd = new Etcd(config);
@@ -12,4 +14,28 @@ const main = async () => {
     await Promise.all(array.map(a => etcd._client.put(`${prefix}/${a}`, { timestamp: 1, data })));
 };
 
-main();
+const array_chunks = (array, chunk_size) => Array(Math.ceil(array.length / chunk_size)).fill().map((_, index) => index * chunk_size).map(begin => array.slice(begin, begin + chunk_size));
+const fillRedis = async () => {
+    const useSentinel = !!process.env.REDIS_SENTINEL_SERVICE_HOST;
+    const config = {
+        host: useSentinel ? process.env.REDIS_SENTINEL_SERVICE_HOST : process.env.REDIS_SERVICE_HOST || 'localhost',
+        port: useSentinel ? process.env.REDIS_SENTINEL_SERVICE_PORT : process.env.REDIS_SERVICE_PORT || 6379,
+        sentinel: useSentinel,
+    }
+    const redis = Factory.getClient(config);
+
+    const prefix = '/hkube:pipeline:graph';
+    const count = 100;
+    const dataSize = 50;
+    const data = Buffer.alloc(dataSize, 'x').toString();
+    const array = Array.from(Array(count).keys());
+    let counter = 0;
+    for (const chunk of array_chunks(array,300)){
+        await Promise.all(chunk.map(a => redis.set(`${prefix}/${a}`, JSON.stringify({ timestamp: 1, data }))));
+        counter = counter + 300;
+        console.log(`wrote ${counter}`)
+    }
+    console.log('Done')
+};
+
+fillRedis();
